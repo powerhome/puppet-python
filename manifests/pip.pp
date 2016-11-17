391a6b05dbc24823ae2f8e4a6871237ef0178e94
David Taylor <djtaylor13@gmail.com>
2016-11-17 12:50:13 -0500

    Better debugging

--- manifests/pip.pp
+++ manifests/pip.pp
@@ -189,6 +189,7 @@ define python::pip (
 
   # Explicit version out of VCS when PIP supported URL is provided
   if $source =~ /^(git\+|hg\+|bzr\+|svn\+)(http|https|ssh|svn|sftp|ftp|lp)(:\/\/).+$/ {
+    notify { "Install from version out of VCS": }
     case $ensure {
       latest: {
         # make sure install gets run on each puppet run when installing from a repo.
@@ -228,11 +229,11 @@ define python::pip (
       }
     }
   } else {
+    notify { "Installing Python package: ensure=${ensure}, name=${name}, grep_regex=${grep_regex}, pip_env=${pip_env}": }
     case $ensure {
       /^((19|20)[0-9][0-9]-(0[1-9]|1[1-2])-([0-2][1-9]|3[0-1])|[0-9]+\.\w+\+?\w*(\.\w+)*)$/: {
         # Version formats as per http://guide.python-distribute.org/specification.html#standard-versioning-schemes
         # Explicit version.
-        notify { "Installing Python package: name=${name}, grep_regex=${grep_regex}, pip_env=${pip_env}": }
         exec { "pip_install_${name}":
           command     => "${pip_env} wheel --help > /dev/null 2>&1 && { ${pip_env} wheel --version > /dev/null 2>&1 || wheel_support_flag='--no-use-wheel'; } ; { ${pip_env} --log ${log}/pip.log install ${install_args} \$wheel_support_flag ${pypi_index} ${proxy_flag} ${install_args} ${install_editable} ${source}==${ensure} || ${pip_env} --log ${log}/pip.log install ${install_args} ${pypi_index} ${proxy_flag} ${install_args} ${install_editable} ${source}==${ensure} ;}",
           unless      => "${pip_env} freeze | grep -i -e ${grep_regex} || ${pip_env} list | sed -e 's/[ ]\\+/==/' -e 's/[()]//g' | grep -i -e ${grep_regex}",
